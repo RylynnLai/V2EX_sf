07862d7e503d060b0807662fb6f20f01f87b642d
macbook air <LLZ@llzdeimac.local>
2016-04-26 13:46:46 +0800

    初始化

---
+++ V2EXTests/V2EXTests.swift
@@ -0,0 +1,36 @@
+//
+//  V2EXTests.swift
+//  V2EXTests
+//
+//  Created by LLZ on 16/4/26.
+//  Copyright © 2016年 LLZ. All rights reserved.
+//
+
+import XCTest
+@testable import V2EX
+
+class V2EXTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
